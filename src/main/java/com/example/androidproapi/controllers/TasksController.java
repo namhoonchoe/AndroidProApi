c7fd05d2b3e674d0a5a9adf4baf4af692f17f1c3
Namhoon Choe <sysynh2012@gmail.com>
2023-12-08 15:05:08 +0900

    [FEAT] Task Controller
     Task Controller에 있는 모든 엔드포인트 포스트맨 확인 완료!

--- src/main/java/com/example/androidproapi/controllers/TasksController.java
+++ src/main/java/com/example/androidproapi/controllers/TasksController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/boards")
+@RequestMapping("/api/")
 public class TasksController {
 
     private TaskService taskService;
@@ -19,4 +19,28 @@ public class TasksController {
     public TasksController(TaskService taskService) {
         this.taskService = taskService;
     }
-}
+
+    @GetMapping("boards/{boardId}/tasks")
+    public List<TaskDto> getAllTasksByBoardId(@PathVariable(value = "boardId") Long boardId) {
+        return taskService.getAllTasksByBoardId(boardId);
+    }
+    @PostMapping("boards/{boardId}/tasks")
+    public ResponseEntity<TaskDto> createTask(@PathVariable(value = "boardId") Long boardId, @RequestBody TaskDto taskDto) {
+        return new ResponseEntity<>(taskService.createTask(boardId, taskDto), HttpStatus.CREATED); //(taskDto, HttpStatus.CREATED)
+    }
+    @GetMapping("boards/{boardId}/tasks/{taskId}")
+    public ResponseEntity<TaskDto> getTaskById(@PathVariable(value = "boardId") Long boardId, @PathVariable(value = "taskId") Long taskId) {
+        TaskDto taskDto = taskService.getTaskByTaskId(boardId, taskId);
+        return new ResponseEntity<>(taskDto, HttpStatus.OK); //(taskDto, HttpStatus.OK)
+    }
+    @PutMapping("boards/{boardId}/tasks/{taskId}")
+    public ResponseEntity<TaskDto> updateTaskById(@PathVariable(value = "boardId") Long boardId, @PathVariable(value = "taskId") Long taskId, @RequestBody TaskDto taskDto) {
+        TaskDto updatedTask = taskService.updateTaskByTaskId(boardId, taskId, taskDto);
+        return new ResponseEntity<>(updatedTask, HttpStatus.OK); //(updatedTask, HttpStatus.OK)
+    }
+    @DeleteMapping("boards/{boardId}/tasks/{taskId}")
+    public ResponseEntity<String> deleteTaskById(@PathVariable(value = "boardId") Long boardId, @PathVariable(value = "taskId") Long taskId) {
+            taskService.deleteTaskByTaskId(boardId, taskId);
+        return new ResponseEntity<>("task deleted successfully", HttpStatus.OK);
+    }
+    }
